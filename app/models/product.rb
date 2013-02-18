188abc7cc72b09f40533b7515bd0eccb3702091b
diguage <leejun119@gmail.com>
2013-02-18 20:44:59 +0800

    澧炲姞鏁版嵁鏍￠獙淇℃伅

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+  validates :image_url, :format => {
+	  :with => %r{\.(gif|jpg|png)}i,
+	  :message => 'Must be a URL for GIF, JPG or PNG image.'
+  }
 end
