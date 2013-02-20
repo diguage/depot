a60a4048c4642dee268a448093cb4d9537a60583
diguage <leejun119@gmail.com>
2013-02-20 14:23:12 +0800

    鍦ㄨ嚜瀹氫箟棣栭〉鏄剧ず浜у搧鍒楄〃

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
+
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
