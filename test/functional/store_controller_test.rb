e12d8c75b3b59152ee785c7d28f22642041a67c0
diguage <leejun119@gmail.com>
2013-02-20 16:08:47 +0800

    澧炲姞Functional娴嬭瘯

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+	assert_select '#columns #side a', :minimum => 4
+	assert_select '#main .entry', 3
+	assert_select 'h3', 'Programming Ruby 1.9'
+	assert_select '.price', /\$[,\d]+\.\d\d/
   end
 
 end
