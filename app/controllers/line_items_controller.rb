781059e276ecb7e12340cf41e79b587ad70cb6b6
diguage <leejun119@gmail.com>
2013-02-21 09:26:02 +0800

    格式化代码显示，检查代码的正确性。

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -40,15 +40,24 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @line_item = LineItem.new(params[:line_item])
+    @cart = current_cart
+    product = Product.find(params[:product_id])
+    @line_item = @cart.line_items.build(:product => product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
-        format.json { render json: @line_item, status: :created, location: @line_item }
+        format.html { redirect_to(@line_item.cart,
+          :notice => 'Line item was successfully created.') }
+        format.xml  { render :xml => @line_item,
+          :status => :created, :location => @line_item }
+        #format.json { render json: @line_item, 
+		#status: :created, location: @line_item }
       else
-        format.html { render action: "new" }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @line_item.errors,
+          :status => :unprocessable_entity }
+        # format.json { render json: @line_item.errors, 
+		# status: :unprocessable_entity }
       end
     end
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,17 +4,8 @@ class Product < ActiveRecord::Base
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
-  # ensure that there are no line items referencing this product
-  def ensure_not_referenced_by_any_line_item
-	  if line_items.count.zero?
-		  return true
-	  else
-		  errors[:base] << "Line Items present"
-		  return false
-	  end
-  end
-
   attr_accessible :description, :image_url, :price, :title
+
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
@@ -22,4 +13,16 @@ class Product < ActiveRecord::Base
 	  :with => %r{\.(gif|jpg|png)}i,
 	  :message => 'Must be a URL for GIF, JPG or PNG image.'
   }
+
+  private
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+        if line_items.count.zero?
+      	  return true
+        else
+      	  errors[:base] << "Line Items present"
+      	  return false
+        end
+    end
+
 end
