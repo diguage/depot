5e8d7680fa71a57f71c44788f3aeadf9ec527ead
diguage <leejun119@gmail.com>
2013-02-21 20:59:04 +0800

    修改已有模型，增加属性

---
+++ db/migrate/20130221124232_add_quantity_to_line_item.rb
@@ -0,0 +1,9 @@
+class AddQuantityToLineItem < ActiveRecord::Migration
+  def change
+    add_column :line_items, :quantity, :integer, :default => 1
+  end
+
+  def self.down
+    remove_column :line_items, :quantity
+  end
+end
