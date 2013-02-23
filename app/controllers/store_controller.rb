6e53e31226b08c9a6ff87f84d0130dc65d62e858
diguage <leejun119@gmail.com>
2013-02-23 14:12:04 +0800

    将master分支删除，新建3.2版的，从头开始

--- app/controllers/store_controller.rb
+++
@@ -1,6 +0,0 @@
-class StoreController < ApplicationController
-  def index
-	  @products = Product.all
-	  @cart = current_cart
-  end
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Depot::Application.config.secret_token = '3a799e457f569f5c8f64960e30d2a749f94a30163efe30bfe58e0eb27e87702f19ed6a8e62639f1baa4e278fd83064856238ca97aa0b23b1311a884458f80f40'
