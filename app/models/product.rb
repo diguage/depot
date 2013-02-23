a9b60bd65aced3baa95849f3b49e2b5aecb5b871
diguage <leejun119@gmail.com>
2013-02-23 23:57:25 +0800

    校验数据唯一性

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
   validates :description, :image_url, :title, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
 end
