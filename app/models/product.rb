2edbefdad6034c05d7871bf9ff80d53e2822c393
diguage <leejun119@gmail.com>
2013-02-24 00:05:17 +0800

    使用正则表达式检查图片的后缀

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,8 @@ class Product < ActiveRecord::Base
   validates :description, :image_url, :title, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
+  validates :image_url, allow_blank: ture, format: {
+    with: %r{\.(gif|jpg|png)$}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
