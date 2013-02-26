4f285c3758ce89f38243930778858085e736c5c4
diguage <leejun119@gmail.com>
2013-02-26 15:52:24 +0800

    检查空Product对象是否可用以及每个属性是否包含错误

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "product attributes must not be empty" do
+	  product = Product.new
+	  assert product.invalid?
+	  assert product.errors[:title].any?
+	  assert product.errors[:description].any?
+	  assert product.errors[:price].any?
+	  assert product.errors[:image_url].any?
   end
 end
