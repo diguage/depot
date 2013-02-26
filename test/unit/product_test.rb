be497b0eea5283e970996918ba5ff2f6a5e29bf1
diguage <leejun119@gmail.com>
2013-02-26 18:00:27 +0800

    瀵筆roduct鐨刾rice灞炴€ц繘琛屾祴璇曪紝

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -9,4 +9,22 @@ class ProductTest < ActiveSupport::TestCase
 	  assert product.errors[:price].any?
 	  assert product.errors[:image_url].any?
   end
+  test "product price must be positive" do
+	  product = Product.new(title: "My Book Title",
+						    description: "yyy",
+						    image_url: "zzz.jpg")
+
+	  product.price = -1
+	  assert product.invalid?
+	  assert_equal "must be greater than or equal to 0.01", 
+		  product.errors[:price].join('; ');
+
+	  product.price = 0
+	  assert product.invalid?
+	  assert_equal "must be greater than or equal to 0.01", 
+		  product.errors[:price].join('; ');
+
+	  product.price = 1
+	  assert product.valid?
+  end
 end
