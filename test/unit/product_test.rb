824f6c70940d3be0f9203709c8ac6f21c70d0da5
diguage <leejun119@gmail.com>
2013-02-26 18:07:36 +0800

    增加图片URL的测试方法

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -33,4 +33,16 @@ class ProductTest < ActiveSupport::TestCase
 				  price: 1,
 				  image_url: image_url)
   end
+  test "image url" do
+	  ok = %w{fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg 
+		         http://a.b.c/x/y/z/fred.gif}
+	  bad = %w{fred.doc fred.gif/more fred.gif.more }
+	  ok.each do |name|
+		  assert new_product(name).valid?, "#{name} shouldn't be invalid"
+	  end
+
+	  bad.each do |name|
+		  assert new_product(name).invalid?, "#{name} shouldn't be valid"
+	  end
+  end
 end
