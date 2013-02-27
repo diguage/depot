6284c8652a8bc7755a5533c7829a8973effa2ac0
diguage <leejun119@gmail.com>
2013-02-27 12:55:50 +0800

    增加错误信息的国际化支持

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -60,4 +60,13 @@ class ProductTest < ActiveSupport::TestCase
 	  assert !product.save
 	  assert_equal "has already been taken", product.errors[:title].join('; ')
   end
+  test "product is not valid without a unique title - i18n" do
+	  product = Product.new(title: products(:ruby).title,
+						    description: "yyy",
+						    price: 1,
+						    image_url: "fred.gif")
+	  assert !product.save
+	  assert_equal I18n.translate('activerecord.errors.messages.taken'), 
+		  product.errors[:title].join('; ')
+  end
 end
