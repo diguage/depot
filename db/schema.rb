36d29585dca439ccdaf2f4e564e45aecd8dfdf80
diguage <leejun119@gmail.com>
2013-02-28 22:05:34 +0800

    鐢熸垚cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130223064052) do
+ActiveRecord::Schema.define(:version => 20130228140323) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"
