7a450c7252a181e79899f6ebe6beb2986a48627f
diguage <leejun119@gmail.com>
2013-03-01 14:14:47 +0800

    建立对象之间的关联关系

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,18 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)$}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+   def ensure_not_referenced_by_any_line_item
+     if line_items.empty?
+       return true
+	 else
+	   errors.add(:base, 'Line Items present')
+	   return false
+	 end
+   end
 end
