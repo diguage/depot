3189ecdf4a62a172fdc3142c146a813e41ec7844
diguage <leejun119@gmail.com>
2013-03-03 00:33:10 +0800

    添加创建LineItem的代码

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -40,11 +40,14 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @line_item = LineItem.new(params[:line_item])
+	@cart = current_cart
+	product = Product.find(params[:product_id])
+    @line_item = @cart.line_items.build(product: product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart, 
+			notice: 'Line item was successfully created.' }
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
