e0fdb5c1b83f59a2f57c123e894d1b50f8fe1497
diguage <leejun119@gmail.com>
2013-03-16 06:35:34 +0800

    在购物车中增加方法，使用新增的一列

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,13 @@
 class Cart < ActiveRecord::Base
   # attr_accessible :title, :body
   has_many :line_items, dependent: :destroy
+  def add_product(product_id)
+	  current_item = line_items.find_by_product_id(product_id)
+	  if current_item
+		  current_item.quantity += 1
+	  else
+		  current_item = line_items.build(product_id: product_id)
+	  end
+	  current_item
+  end
 end
