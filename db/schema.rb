3903fea2289a9392e68072f5b21f3a6f897d304e
diguage <leejun119@gmail.com>
2013-03-16 15:01:18 +0800

    执行完remove_XXX_from_TABLE方法后，migrate后的效果

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130315224434) do
+ActiveRecord::Schema.define(:version => 20130316065522) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
