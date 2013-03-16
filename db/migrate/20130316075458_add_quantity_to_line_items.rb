ab664a42dac200cc62a300820bcbd6669709a216
diguage <leejun119@gmail.com>
2013-03-16 15:56:13 +0800

    由于忘记添加默认值，把原来的列删掉，然后再次添加

--- db/migrate/20130316075458_add_quantity_to_line_items.rb
+++ db/migrate/20130316075458_add_quantity_to_line_items.rb
@@ -1,5 +1,5 @@
 class AddQuantityToLineItems < ActiveRecord::Migration
   def change
-    add_column :line_items, :quantity, :integer
+    add_column :line_items, :quantity, :integer, default: 1
   end
 end
