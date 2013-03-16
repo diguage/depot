f651a243e840ed37294e2a2b811161323a0c71f4
diguage <leejun119@gmail.com>
2013-03-16 15:12:24 +0800

    执行完rails generate migration remove_quantity_from_line_items quantity后，再执行rake db:migrate产生的效果

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130316065522) do
+ActiveRecord::Schema.define(:version => 20130316070525) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -21,9 +21,8 @@ ActiveRecord::Schema.define(:version => 20130316065522) do
   create_table "line_items", :force => true do |t|
     t.integer  "product_id"
     t.integer  "cart_id"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
-    t.integer  "quantity",   :default => 1
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "products", :force => true do |t|
