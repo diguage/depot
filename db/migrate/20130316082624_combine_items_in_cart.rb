c8b21a754300b539e6e3a89684edb7af694a7cb4
diguage <leejun119@gmail.com>
2013-03-17 15:10:07 +0800

    合并购物车中相同商品的记录，添加回退方法的实现

--- db/migrate/20130316082624_combine_items_in_cart.rb
+++ db/migrate/20130316082624_combine_items_in_cart.rb
@@ -17,5 +17,16 @@ class CombineItemsInCart < ActiveRecord::Migration
   end
 
   def down
+	  # split items with quantity > 1 into multiple items
+	  LineItem.where("quantity>1").each do |line_item|
+		  # add individual items
+		  line_item.quantity.times do
+			  LineItem.create cart_id: line_item.cart_id,
+				  product_id: line_item.product_id, quantity: 1
+		  end
+
+		  # remove original item
+		  line_item.destroy
+	  end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130316075458) do
+ActiveRecord::Schema.define(:version => 20130316082624) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
