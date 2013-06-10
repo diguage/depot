731162ce39071a68c94702fdef1ad43688bf4ec0
diguage <leejun119@gmail.com>
2013-06-10 14:55:49 +0800

    澧炲姞璁㈠崟澶勭悊鏂规硶

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,6 +24,12 @@ class OrdersController < ApplicationController
   # GET /orders/new
   # GET /orders/new.json
   def new
+    @cart = current_cart
+    if @cart.line_items.empty?
+      redirect_to store_url, notice: "Your cart is empty"      
+      return
+    end
+
     @order = Order.new
 
     respond_to do |format|
