43ee8d6d54bedb84966cce703ab86b5ee3d4af41
diguage <leejun119@gmail.com>
2013-06-10 15:36:26 +0800

    澧炲姞璁㈠崟鏍￠獙

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ActiveRecord::Base
   attr_accessible :address, :email, :name, :pay_type
   PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
 end
