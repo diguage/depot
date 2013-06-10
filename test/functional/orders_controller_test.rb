422c6180084e1c2fa8fe24eca4e0d1dff6d7599b
diguage <leejun119@gmail.com>
2013-06-10 14:56:06 +0800

    澧炲姞娴嬭瘯鏂规硶

--- test/functional/orders_controller_test.rb
+++ test/functional/orders_controller_test.rb
@@ -11,7 +11,17 @@ class OrdersControllerTest < ActionController::TestCase
     assert_not_nil assigns(:orders)
   end
 
+  test "require item in cart" do
+    get :new
+    assert_redirected_to store_path
+    assert_equal flash[:notice], 'Your cart is empty'
+  end
+
   test "should get new" do
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    LineItem.create(cart: cart, product: products(:ruby))
+
     get :new
     assert_response :success
   end
