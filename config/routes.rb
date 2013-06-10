2c6fbca11b5cfaee051f7d3cbadc6f417259f264
diguage <leejun119@gmail.com>
2013-06-11 07:59:08 +0800

    增加Atom的路由功能

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Depot::Application.routes.draw do
 
   get "store/index"
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
 
   # The priority is based upon order of creation:
