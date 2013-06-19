2d51df0dff05eccfb89201990d5cc03a6415d490
diguage <leejun119@gmail.com>
2013-06-19 22:58:25 +0800

    修改发件人Email

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -1,5 +1,5 @@
 class OrderNotifier < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "Dgg <depot@diguage.com>"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
